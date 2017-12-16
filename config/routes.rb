fd81472ff8b217d6c0fb79c0fca20518cc3e6e2f
sanctusdecadere <emiliosoto4@yahoo.com>
2017-12-16 14:23:44 -0500

    Changed routing structure for static pages, removed resources routing and linked nav and footer menu link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-	resources :welcome
-	root 'welcome#index'
+
+	root "welcome#show", page: "index"
+
+	get "/welcome/:page" => "welcome#show"
+
+
+
+
+
+
+
+	# root 'welcome#index'
+
+	# resources :welcome do 
+	# 	collection do
+	# 		patch :menu
+	# 	end
+	# end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

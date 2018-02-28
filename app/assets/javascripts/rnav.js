1f9e83e8bc16176de0195ce6518ba59e4723a6dd
sanctusdecadere <emiliosoto4@yahoo.com>
2018-02-28 17:55:24 -0500

    Successfully added Image Slider to gallery page

--- app/assets/javascripts/rnav.js
+++ app/assets/javascripts/rnav.js
@@ -1,8 +1,12 @@
 function myFunction() {
+	
     var x = document.getElementById("myNavitem");
     if (x.className === "nav-item") {
         x.className += " responsive";
     } else {
         x.className = "nav-item";
     }
+
+
 }
+
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
 
 
 
+
 	# root 'welcome#index'
 
 	# resources :welcome do 

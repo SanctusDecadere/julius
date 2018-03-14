b29bdcfa31cd69aa366d123d825276fca2b44ded
sanctusdecadere <emiliosoto4@yahoo.com>
2018-03-14 17:18:01 -0400

    Added links to the link page and basic styling

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,7 @@
 class ContactMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.new_comment.subject
-  #
   def new_comment
-   # @comment = comment
-   # @item = @comment.item
+
     mail to: "emiliosoto4@gmail.com",
     	subject: "You have visited the Julius Website"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
 	get "welcome/contact"
 
+	get "welcome/links"
+
 
 
 	# root 'welcome#index'

4da4d4eececaa884fcff1be66ac1910938f00fe7
sanctusdecadere <emiliosoto4@yahoo.com>
2018-05-07 22:07:29 -0400

    no

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,4 +1,4 @@
-AWS::S3::Base.establish_connection!(
- :access_key_id   => ENV['SENDGRID_USERNAME'],
- :secret_access_key => ENV['SENDGRID_PASSWORD']
-)
+# AWS::S3::Base.establish_connection!(
+#  :access_key_id   => ENV['SENDGRID_USERNAME'],
+#  :secret_access_key => ENV['SENDGRID_PASSWORD']
+# )

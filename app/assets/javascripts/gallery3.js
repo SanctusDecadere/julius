0ac77afc5d29e176a893c9327c13e570538abd8a
sanctusdecadere <emiliosoto4@yahoo.com>
2018-07-09 01:32:52 -0400

    Adjusted slide left and right button but still trying to get the main image to slide and not the small one in mini gallery

--- app/assets/javascripts/gallery3.js
+++ app/assets/javascripts/gallery3.js
@@ -1,111 +1,82 @@
+window.onload = function begin() {
+	let sliderImages = document.querySelectorAll(".testImage"),
+		arrowLeft = document.querySelector("#arrow-left"),
+		arrowRight = document.querySelector("#arrow-right"),
+		actualMainImage = document
+			.querySelector("#actualMainImage")
+			.getAttribute("src");
+	// Represents what image we are on. 0 by default.
+	current = 0;
 
-// All Images In Gallery
-var	testImages = document.querySelectorAll('.testImage').getAttribute("src");
+	// 	**********************************************
 
-// Previous and Next Image Buttons
-var prevButton = document.querySelector('#prev-button');
-var	nextButton = document.querySelector('#next-button');
-
-// Array of Images in Gallery Via Index.
-var	currently = testImages[i];
-
-var i;
-
-var actualMainImage = document.querySelector('.actualMainImage');
-
-// *****************************************************
-
-// Clears all images
-function reset() {
-	for( i = 0; i < testImages.length; i++) {
-			// testImages[i].style.display = 'none';
+	// Clears all images.
+	function reset() {
+		for (let i = 0; i < sliderImages.length; i++) {
+			sliderImages[i].style.display = "none";
+		}
 	}
-}
 
-// *****************************************************
-// Initializes the slider
-function startSlide() {
-	reset();
-	testImages[0].style.display = 'block';
-}
+	// 	**********************************************
 
-// *****************************************************
-// Show Prev
-function prevImage() {
-	reset();
-	actualMainImage.src = currently;
-
-	testImages[currently - 1].style.display = 'block';
-	currently--;
-}
-
-
-// Left Arrow Click
-window.onload = function() {
-	var prevButton = document.querySelector('#prev-button');
-	for( let i = 0; i < testImages.length; i++) {
-		prevButton[i].addEventListener('click', function() {
-				if(currently === 0) {
-					currently = testImages.length;
-				}
-	prevImage();
-		});
+	// Initializer
+	function startSlide() {
+		reset();
+		sliderImages[0].style.display = "block";
 	}
+
+	// 	**********************************************
+
+	// Show Prev
+	function slideLeft() {
+		reset();
+		sliderImages[current - 1].style.display = "block";
+		current--;
+	}
+
+	// left Arrow Click
+	arrowLeft.addEventListener("click", function() {
+		if (current === 0) {
+			current = sliderImages.length;
+		}
+		slideLeft();
+	});
+	// 	**********************************************
+
+	// 	**********************************************
+	// Show Next
+	function slideRight() {
+		reset();
+		sliderImages[current + 1].style.display = "block";
+		current++;
+	}
+
+	// left Arrow Click
+	arrowRight.addEventListener("click", function() {
+		if (current === sliderImages.length - 1) {
+			current = -1;
+		}
+		slideRight();
+	});
+	// 	**********************************************
+
+	startSlide();
 };
-
-// prevButton.addEventListener('click', function() {
-// 	if(currently === 0) {
-// 		currently = testImages.length;
-// 	}
-// 	prevImage();
-// });
-
-// *****************************************************
-
-// Show Next
-function nextImage() {
-	reset();
-	actualMainImage.src = currently;
-
-	testImages[currently + 1].style.display = 'block';
-	currently++;
-}
-
-// Right Arrow Click
-window.onload = function() {
-	var nextButton = document.querySelector('#next-button');
-	for( let i = 0; i < testImages.length; i++) {
-		nextButton[i].addEventListener('click', function() {
-				if(currently === testImages.length -1) {
-					currently = -1
-				}
-	nextImage();
-		});
-	}
-};
-
-
-// Right Arrow Click
-// nextButton.addEventListener('click', function() {
-// 	if(currently === testImages.length - 1) {
-// 		currently = -1
-// 	}
-// 	nextImage();
-// });
-
-// *****************************************************
-
-// startSlide();
 
 // *****************************************************
 // Initial Image Gallery
 
-	// Example of ES6 Destructuring. This can be used instead of line 41 and 42. Saves 1 line of code. 
-	var [current, imgs] = [document.querySelector('#current'), document.querySelectorAll('.imgs img') ];
+// Example of ES6 Destructuring. This can be used instead of line 41 and 42. Saves 1 line of code.
+var [current, imgs] = [
+	document.querySelector("#current"),
+	document.querySelectorAll(".imgs img")
+];
 
-	// const current = document.querySelector('#current');
-	// const imgs = document.querySelectorAll('.imgs img');
+// const current = document.querySelector('#current');
+// const imgs = document.querySelectorAll('.imgs img');
 
-	imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
+imgs.forEach(img =>
+	img.addEventListener("click", e => (current.src = e.target.src))
+);
 
 // *****************************************************

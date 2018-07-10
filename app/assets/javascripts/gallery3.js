3c3d4193e193a7b702b4ab37b61c77e1b194f369
sanctusdecadere <emiliosoto4@yahoo.com>
2018-07-10 12:55:56 -0400

    changed some styling and added max width for imgs container

--- app/assets/javascripts/gallery3.js
+++ app/assets/javascripts/gallery3.js
@@ -1,67 +1,67 @@
-window.onload = function begin() {
-	let sliderImages = document.querySelectorAll(".testImage"),
-		arrowLeft = document.querySelector("#arrow-left"),
-		arrowRight = document.querySelector("#arrow-right"),
-		actualMainImage = document
-			.querySelector("#actualMainImage")
-			.getAttribute("src");
-	// Represents what image we are on. 0 by default.
-	current = 0;
+// window.onload = function begin() {
+// 	// let sliderImages = document.querySelectorAll(".testImage"),
+// 	let sliderImages = document.querySelectorAll(".container img"),
+// 		arrowLeft = document.querySelector("#arrow-left"),
+// 		arrowRight = document.querySelector("#arrow-right"),
+// 		actualMainImage = document.querySelector("#current")
+		
+// 	// Represents what image we are on. 0 by default.
+// 	current = 0;
 
-	// 	**********************************************
+// 	// 	**********************************************
 
-	// Clears all images.
-	function reset() {
-		for (let i = 0; i < sliderImages.length; i++) {
-			sliderImages[i].style.display = "none";
-		}
-	}
+// 	// Clears all images.
+// 	function reset() {
+// 		for (let i = 0; i < sliderImages.length; i++) {
+// 			sliderImages[i].style.display = "block";
+// 		}
+// 	}
 
-	// 	**********************************************
+// 	// 	**********************************************
 
-	// Initializer
-	function startSlide() {
-		reset();
-		sliderImages[0].style.display = "block";
-	}
+// 	// Initializer
+// 	function startSlide() {
+// 		reset();
+// 		sliderImages[0].style.display = "block";
+// 	}
 
-	// 	**********************************************
+// 	// 	**********************************************
 
-	// Show Prev
-	function slideLeft() {
-		reset();
-		sliderImages[current - 1].style.display = "block";
-		current--;
-	}
+// 	// Show Prev
+// 	function slideLeft() {
+// 		reset();
+// 		sliderImages[current - 1].style.display = "block";
+// 		current--;
+// 	}
 
-	// left Arrow Click
-	arrowLeft.addEventListener("click", function() {
-		if (current === 0) {
-			current = sliderImages.length;
-		}
-		slideLeft();
-	});
-	// 	**********************************************
+// 	// left Arrow Click
+// 	arrowLeft.addEventListener("click", function() {
+// 		if (current === 0) {
+// 			current = sliderImages.length;
+// 		}
+// 		slideLeft();
+// 	});
+// 	// 	**********************************************
 
-	// 	**********************************************
-	// Show Next
-	function slideRight() {
-		reset();
-		sliderImages[current + 1].style.display = "block";
-		current++;
-	}
+// 	// 	**********************************************
+// 	// Show Next
+// 	function slideRight() {
+// 		reset();
+// 		sliderImages[current + 1].style.display = "block";
+// 		current++;
+// 	}
 
-	// left Arrow Click
-	arrowRight.addEventListener("click", function() {
-		if (current === sliderImages.length - 1) {
-			current = -1;
-		}
-		slideRight();
-	});
-	// 	**********************************************
+// 	// left Arrow Click
+// 	arrowRight.addEventListener("click", function() {
+// 		if (current === sliderImages.length - 1) {
+// 			current = -1;
+// 		}
+// 		slideRight();
+// 	});
+// 	// 	**********************************************
 
-	startSlide();
-};
+// 	startSlide();
+// };
 
 // *****************************************************
 // Initial Image Gallery

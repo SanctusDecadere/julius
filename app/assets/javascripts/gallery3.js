e879714d6c962e244ef65da400e8899cc3daca4b
sanctusdecadere <emiliosoto4@yahoo.com>
2018-10-10 12:19:18 -0400

    restyled gallery page

--- app/assets/javascripts/gallery3.js
+++ app/assets/javascripts/gallery3.js
@@ -1,82 +1,14 @@
-// window.onload = function begin() {
-// 	// let sliderImages = document.querySelectorAll(".testImage"),
-// 	let sliderImages = document.querySelectorAll(".container img"),
-// 		arrowLeft = document.querySelector("#arrow-left"),
-// 		arrowRight = document.querySelector("#arrow-right"),
-// 		actualMainImage = document.querySelector("#current")
-		
-// 	// Represents what image we are on. 0 by default.
-// 	current = 0;
-
-// 	// 	**********************************************
-
-// 	// Clears all images.
-// 	function reset() {
-// 		for (let i = 0; i < sliderImages.length; i++) {
-// 			sliderImages[i].style.display = "block";
-// 		}
-// 	}
-
-// 	// 	**********************************************
-
-// 	// Initializer
-// 	function startSlide() {
-// 		reset();
-// 		sliderImages[0].style.display = "block";
-// 	}
-
-// 	// 	**********************************************
-
-// 	// Show Prev
-// 	function slideLeft() {
-// 		reset();
-// 		sliderImages[current - 1].style.display = "block";
-// 		current--;
-// 	}
-
-// 	// left Arrow Click
-// 	arrowLeft.addEventListener("click", function() {
-// 		if (current === 0) {
-// 			current = sliderImages.length;
-// 		}
-// 		slideLeft();
-// 	});
-// 	// 	**********************************************
-
-// 	// 	**********************************************
-// 	// Show Next
-// 	function slideRight() {
-// 		reset();
-// 		sliderImages[current + 1].style.display = "block";
-// 		current++;
-// 	}
-
-// 	// left Arrow Click
-// 	arrowRight.addEventListener("click", function() {
-// 		if (current === sliderImages.length - 1) {
-// 			current = -1;
-// 		}
-// 		slideRight();
-// 	});
-// 	// 	**********************************************
-
-// 	startSlide();
-// };
 
 // *****************************************************
 // Initial Image Gallery
-
 // Example of ES6 Destructuring. This can be used instead of line 41 and 42. Saves 1 line of code.
 var [current, imgs] = [
 	document.querySelector("#current"),
 	document.querySelectorAll(".imgs img")
 ];
 
-// const current = document.querySelector('#current');
-// const imgs = document.querySelectorAll('.imgs img');
 
 imgs.forEach(img =>
 	img.addEventListener("click", e => (current.src = e.target.src))
 );
-
 // *****************************************************

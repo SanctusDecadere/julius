ce47d5bd423827a5b51ddc6bf4353bd82ce8e0f8
sanctusdecadere <emiliosoto4@yahoo.com>
2018-10-10 13:34:10 -0400

    added lines below mobile nav links and an oval around arrow button

--- app/assets/javascripts/rnav.js
+++ app/assets/javascripts/rnav.js
@@ -1,12 +1,22 @@
-function myFunction() {
-	
-    var x = document.getElementById("myNavitem");
-    if (x.className === "navbar-links") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-links";
-    }
 
 
-}
+	function myFunction() {
+		
+	    var x = document.getElementById("myNavitem");
+	    if (x.className === "navbar-links") {
+	        x.className += " responsive";
+	        // x.slideDown(3000);
+	    } else {
+	        x.className = "navbar-links";
+	    }
+	   
 
+	}
+
+
+
+// $('document').ready(function() {
+// 	console.log('work bitch');
+
+// 	document.querySelector('navbar-links').slideDown(3000);
+// })
